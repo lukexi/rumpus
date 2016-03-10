2600c68fb1be1ce1842b704c1e7b17c199b01819
Luke Iannini <lukexi@me.com>
2016-03-10 03:42:39 -0800

    Haptics sketch

---
+++ src/Rumpus/Systems/Haptics.hs
@@ -0,0 +1,37 @@
+module Rumpus.Systems.Haptics where
+
+import Rumpus.Systems.Controls
+import Rumpus.Systems.Physics
+import Rumpus.Systems.Shared
+import PreludeExtra
+
+data HapticsSystem = HapticsSystem 
+    { _hptLastHandTick :: Map WhichHand (V3 GLfloat)
+    } deriving Show
+makeLenses ''HapticsSystem
+
+defineSystemKey ''HapticsSystem
+
+
+initHapticsSystem :: MonadState ECS m => VRPal -> m ()
+initHapticsSystem vrPal = do
+    registerSystem sysHaptics $ HapticsSystem
+            { _hptLastHandTick = mempty
+            }
+
+beginHapticDrag = tickWithPose
+
+continueHapticDrag whichHand pose = do
+    mLastPose <- viewSystem sysHaptics (hptLastHandTick . at whichHand)
+    case mLastPose of
+        Just lastPose
+            | lastPose - pose > 10 -> 
+                tickWithPose whichHand pose
+        _ -> return ()
+
+tickWithPose whichHand pose = do
+    modifySystem sysHaptics $ hptLastHandTick . at whichHand ?= pose
+    triggerHapticPulse whichHand 0.1 100
+
+endHapticDrag whichHand = do
+    modifySystem sysHaptics $ hptLastHandTick . at whichHand ?= pose
