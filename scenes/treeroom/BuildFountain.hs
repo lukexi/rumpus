7843905b25cf431abb90c829080120dde257bcb6
Luke Iannini <lukexi@me.com>
2016-03-14 03:46:12 -0700

    Restore code rendering

--- scenes/treeroom/BuildFountain.hs
+++ scenes/treeroom/BuildFountain.hs
@@ -10,7 +10,7 @@ start = do
             childID <- spawnEntity Transient $ do
                 cmpParent ==> parentID
                 cmpPose   ==> mkTransformation (axisAngle (V3 0 0 1) 0.3) pos
-                cmpShapeType              ==> CubeShape
+                cmpShapeType              ==> SphereShape
                 cmpPhysicsProperties      ==> [NoPhysicsShape]
                 cmpInheritParentTransform ==> InheritFull
                 cmpSize                   ==> V3 0.5 0.6 0.6
@@ -22,5 +22,5 @@ start = do
                 branch childID (n - 1) (V3 1 1 0)
                 branch childID (n - 1) (V3 (-1) 1 0)
     rootEntityID <- ask
-    branch rootEntityID (4::Int) (V3 0 1 0)
+    branch rootEntityID (2::Int) (V3 0 1 0)
     return Nothing
