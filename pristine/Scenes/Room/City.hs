a0de9275d78eb2eaa9a279904060eff0ba2d1161
Luke Iannini <lukexi@me.com>
2016-04-01 04:36:13 -0700

    City

--- pristine/Scenes/Room/City.hs
+++ pristine/Scenes/Room/City.hs
@@ -15,18 +15,41 @@ pointsOnSphere (fromIntegral -> n) =
 start :: OnStart
 start = do
     removeChildren
+    createBuildings
+    createStars
+    return Nothing
+
+createBuildings :: EntityMonad ()
+createBuildings = do
+    rootEntityID <- ask
+    let n = 10
+        buildSites = [V3 (x*20) (-100) (z*20) | x <- [-n..n], z <- [-n..n]]
+    forM_ buildSites $ \(V3 x y z) -> do
+        hue <- liftIO randomIO
+        
+        when (x /= 0 && z /= 0) $ void . spawnEntity Transient $ do
+            cmpParent                 ==> rootEntityID
+            cmpPose                   ==> mkTransformation 
+                                            (axisAngle (V3 0 0 1) 0) (V3 x y z)
+            cmpShapeType              ==> CubeShape
+            cmpPhysicsProperties      ==> [NoPhysicsShape]
+            cmpSize                   ==> V3 10 100 10
+
+            cmpColor                  ==> hslColor hue 0.8 0.8
+
+createStars :: EntityMonad ()
+createStars = do
     rootEntityID <- ask
     
-    let numPoints = 30 :: Int
+    let numPoints = 300 :: Int
         sphere = pointsOnSphere numPoints
         hues = map ((/ fromIntegral numPoints) . fromIntegral) [0..numPoints]
     forM_ (zip sphere hues) $ \(pos, hue) -> void $ spawnEntity Transient $ do
         cmpParent                 ==> rootEntityID
         cmpPose                   ==> mkTransformation 
-                                        (axisAngle (V3 0 0 1) 0.3) (pos * 100)
+                                        (axisAngle (V3 0 0 1) 0.3) (pos * 1000)
         cmpShapeType              ==> SphereShape
         cmpPhysicsProperties      ==> [NoPhysicsShape]
         cmpInheritParentTransform ==> InheritFull
-        cmpSize                   ==> V3 0.5 0.5 0.5
+        cmpSize                   ==> 5
         cmpColor                  ==> hslColor hue 0.8 0.8
-    return Nothing
