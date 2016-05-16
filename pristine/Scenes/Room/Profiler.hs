68b1f605672f46d56c04ce2184545d03663b8ceb
Luke Iannini <lukexi@me.com>
2016-05-15 23:43:30 -0700

    WIP profile visualizer

--- pristine/Scenes/Room/Profiler.hs
+++ pristine/Scenes/Room/Profiler.hs
@@ -1,16 +1,31 @@
 module Profiler where
 import Rumpus
-
+import qualified Data.HashMap.Strict as Map
+import System.Metrics
 start :: Start
 start = do
 
-    childID <- spawnChild $ do
-        myTextPose    ==> translateMatrix (V3 0 1 0)
-        myText        ==> ""
-        myInheritTransform ==> InheritPose
-    runEntity childID $ do
-        
-        setRepeatingAction (1/10) $ do
-            sample <- getProfilerSample
-            setText (show sample)
+
+    rootSample <- getProfilerSample
+    let indexedSamples = zip [0..] (Map.toList rootSample)
+    gauges <- Map.fromList <$> forM indexedSamples (\(i, (name, value)) -> do
+        childID <- spawnChild $ do
+            myShape            ==> Cube
+            myProperties       ==> [Holographic]
+            myTextPose         ==> translateMatrix (V3 0 1 0)
+            myText             ==> ""
+            myInheritTransform ==> InheritPose
+        return (name, childID))
+
+    setRepeatingAction (1/10) $ do
+        sample <- getProfilerSample
+        forM_ (Map.toList sample) $ \(name, value) -> do
+            forM_ (Map.lookup name gauges) $ \childID -> do
+                let text = case value of
+                    Counter count -> show count
+                    Gauge level -> show level
+                    Label text -> show text
+                    Distribution stats -> show stats
+                runEntity childID $ setText text
+        setText text
     return ()
