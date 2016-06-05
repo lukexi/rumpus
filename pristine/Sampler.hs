508fbfdac77df3fade04a0688f4d52e44f4adfa9
Luke Iannini <lukexi@me.com>
2016-06-05 16:40:11 -0700

    Sampler fixes

--- pristine/Sampler.hs
+++ pristine/Sampler.hs
@@ -5,6 +5,7 @@ import qualified Data.Vector as V
 
 start :: Start
 start = do
+    setSynthPatch "Sampler.pd"
     
     let numSamples = 32 -- actual generated is 256
     children <- V.generateM numSamples $ \i -> do
@@ -16,8 +17,8 @@ start = do
             myPose             ==> position (V3 x 0 0)
             myProperties       ==> [Holographic]
             myInheritPose      ==> InheritPose
+    mainID <- ask
     button <- spawnChild $ do
-        myPdPatchFile     ==> "Sampler.pd"
         myShape           ==> Cube
         myProperties      ==> [Floating]
         mySize            ==> 0.1
@@ -25,20 +26,19 @@ start = do
         myCollisionStart  ==> \_ _ -> do
             hue <- randomRange (0,1)
             setColor $ colorHSL hue 0.8 0.4
-            sendSynth "record-toggle" 1
+            sendEntitySynth mainID "record-toggle" 1
         myCollisionEnd    ==> \_ -> do
-            sendSynth "record-toggle" 0
-        
-        myUpdate          ==> do
-            fftSample <- V.convert <$> readPdArray "sample-fft" 0 numSamples
-            V.forM_ (V.zip children fftSample) $ \(childID, sample) -> do
-                let val = sample
-                inEntity childID $ do
-                    setSize (0.1 & _yz .~ realToFrac val)
-                    myColor ==> colorHSL (realToFrac val) 0.8 0.4
-            return ()
-    
-                    
+            sendEntitySynth mainID "record-toggle" 0
+     
     attachEntity button
     inEntity button $ 
         setAttachmentOffset (position $ V3 0 0.5 0)
+    
+    myUpdate          ==> do
+        fftSample <- V.convert <$> readPdArray "sample-fft" 0 numSamples
+        V.forM_ (V.zip children fftSample) $ \(childID, sample) -> do
+            let val = sample
+            inEntity childID $ do
+                setSize (0.01 & _yz .~ realToFrac val * 0.1)
+                myColor ==> colorHSL (realToFrac val) 0.8 0.4
+        return ()
