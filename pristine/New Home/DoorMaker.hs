a295991dfaaff9dc852fa5ff46899254f737ad15
Luke Iannini <lukexi@me.com>
2016-06-16 04:56:33 -0700

    Tiles

--- pristine/New Home/DoorMaker.hs
+++ pristine/New Home/DoorMaker.hs
@@ -8,27 +8,30 @@ start = do
 
     let sceneNamesWithNewScene = Nothing : map Just sceneNames
         numItems               = fromIntegral $ length sceneNamesWithNewScene
-        
-    forM_ (zip [0..] sceneNamesWithNewScene) $ \(i, maybeCodePath) -> do
-        let iF = fromIntegral i / numItems
-            hue = iF
-            x = fromIntegral $ i `div` 2 * 2
-            z = fromIntegral $ i `mod` 2 * 4 - 2
-            r = if i `mod` 2 == 0 then 0 else -pi
-            platShift = if i `mod` 2 == 0 then 1 else -1
+        tileColors             = cycle [0,1,1,0]
+    forM_ (zip3 [0..] tileColors sceneNamesWithNewScene) $ \(i, t, mSceneName) -> do
+        let hue       = fromIntegral i / numItems
+            xI        = i `div` 2
+            zI        = i `mod` 2
+            x         = fromIntegral xI * 2
+            z         = fromIntegral zI * 4 - 2
+            r         = if zI == 0 then 0 else -pi
+            platShift = if zI == 0 then 1 else -1
+            tileColor = if t == 0 then 1 else 0.1
+
         spawnChild $ do
-            myBody ==> Animated
+            myBody      ==> Animated
             myBodyFlags ==> [Teleportable]
-            myShape ==> Cube
-            myPose ==> position (V3 x -0.05 (z + platShift))
-            mySize ==> V3 2 0.1 2
-            myColor ==> colorHSL hue 0.5 0.5
+            myShape     ==> Cube
+            myPose      ==> position (V3 x -0.05 (z + platShift))
+            mySize      ==> V3 2 0.1 2
+            myColor     ==> tileColor
         spawnChild $ do
             myBody          ==> Animated
             myStartExpr     ==> ("Door.hs", "start")
-            myPose          ==> positionRotation 
+            myPose          ==> positionRotation
                 (V3 x 1 z)
                 (axisAngle (V3 0 1 0) r)
-            setState (maybeCodePath, hue::Float) 
-    
- 
+            setState (mSceneName, hue::Float)
+
+
