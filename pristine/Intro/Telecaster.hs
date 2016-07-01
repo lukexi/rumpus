f8958f4f80de0bb9092c24b095877d8bfcd54822
Luke Iannini <lukexi@me.com>
2016-06-30 23:51:56 -0700

    Intro stuff

--- pristine/Intro/Telecaster.hs
+++ pristine/Intro/Telecaster.hs
@@ -1,9 +1,9 @@
 module Telecaster where
 import Rumpus
 
-text = [ ""
-       , ""
-       , ""
+text = [ "Welcome to Rumpus"
+       , "A Live-Coding Playground"
+       , "For Room-Scale VR"
        ]
 
 start :: Start
