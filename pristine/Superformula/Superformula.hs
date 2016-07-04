e827dc70fa42599aa49f9d14cc3f765b6f31443b
Luke Iannini <lukexi@me.com>
2016-07-04 04:33:56 -0700

    Restore enveloping superformula

--- pristine/Superformula/Superformula.hs
+++ pristine/Superformula/Superformula.hs
@@ -1,50 +1,7 @@
 module Superformula where
 import Rumpus
 
-
-start :: Start
-start = do
-    let n = 10
-
-    -- Show the 2D superformula
-    -- visualize2D n
-
-    let threeD = [(theta, phi)
-                    | theta <- floats (-pi, pi) n
-                    , phi   <- floats (-pi/2, pi/2) n]
-    spawnChildren threeD $ \(theta, phi) -> do
-        let pos = sphereSuperformula theta phi
-                1 1 3 5 18 18
-                1 1 3 5 18 18
-        --printIO pos
-        myShape ==> Sphere
-        myTransformType ==> AbsolutePose
-        mySize  ==> 0.01
-        myPose  ==> position (pos * 0.2 + V3 0 5 0)
-        myColor ==> colorHSL (phi+theta) 0.5 0.5
-        myUpdate ==> do
-            n <- getNow
-            let now = (sin n + 1) * 0.3 + 0.5
-            setColor $! (colorHSL (phi*now + theta * now) 0.5 0.5)
-            setPosition $! (+ (V3 0 0 0)) . (*0.15) $
-                sphereSuperformula (theta+now) (phi * now)
-                1 1 3 5 18 18
-                1 1 3 5 18 18
-
-    return ()
-
-visualize2D n = do
-    let aSuperformula = superformulaPolar 1 1 3 5 18 18
-    spawnChildren (floats01 n) $ \i -> do
-        let theta = remap (-pi) pi i
-            r     = aSuperformula theta
-            V2 x y = polarToCartesian (r*0.3) theta
-        myShape ==> Cube
-        mySize  ==> 0.01
-        myPose  ==> position (V3 x y -0.5)
-
 -- https://en.wikipedia.org/wiki/Superformula
-
 superformulaPolar a b m n1 n2 n3 phi =
     ( abs (cos (m * phi / 4) / a) ** n2
       +
@@ -64,3 +21,50 @@ sphereSuperformula theta phi
         x      = r1 theta * cos theta * r2 phi * cos phi
         y      = r1 theta * sin theta * r2 phi * cos phi
         z      = r2 phi * sin phi
+
+
+start :: Start
+start = do
+    let n = 15
+        formulaCenter = V3 0 1 0
+        -- Mondo superformula
+        (formulaSize, pointSize) = (5, 0.1)
+        -- Mini superformula
+--        (formulaSize, pointSize) = (0.15, 0.01)
+
+    -- Show the 2D superformula
+    -- visualize2D n
+
+    let threeD = [(theta, phi)
+                    | theta <- floats (-pi, pi) n
+                    , phi   <- floats (-pi/2, pi/2) n]
+    spawnChildren threeD $ \(theta, phi) -> do
+        myShape         ==> Sphere
+        myTransformType ==> AbsolutePose
+        mySize          ==> pointSize
+        myColor         ==> colorHSL (phi+theta) 0.5 0.5
+        myUpdate ==> do
+            n <- getNow
+            let now = (sin n + 1) * 0.3 + 0.5
+            let now = n
+            setColor (colorHSL (phi*now + theta * now) 0.5 0.5)
+            setPosition (
+                sphereSuperformula (theta + now) (phi * now)
+                    1 1 3 5 18 18
+                    1 1 3 5 18 18
+                * formulaSize
+                + formulaCenter
+                )
+
+    return ()
+
+visualize2D n = do
+    let aSuperformula = superformulaPolar 1 1 3 5 18 18
+    spawnChildren (floats01 n) $ \i -> do
+        let theta = remap (-pi) pi i
+            r     = aSuperformula theta
+            V2 x y = polarToCartesian (r*0.3) theta
+        myShape ==> Cube
+        mySize  ==> 0.01
+        myPose  ==> position (V3 x y -0.5)
+
